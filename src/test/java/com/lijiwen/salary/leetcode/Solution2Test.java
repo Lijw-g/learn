82f801666d94752938908af87eeaf2fdc6a86cb6
lijiwen-g <1148226862@qq.com>
2020-02-04 20:59:24 +0800

    change

---
+++ src/test/java/com/lijiwen/salary/leetcode/Solution2Test.java
@@ -0,0 +1,16 @@
+package com.lijiwen.salary.leetcode;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Solution2Test {
+
+
+    @Test
+    public void addTwoNumbers() {
+
+    }
+}
---
+++ src/test/java/com/lijiwen/salary/leetcode/Solution3Test.java
@@ -0,0 +1,14 @@
+package com.lijiwen.salary.leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Solution3Test {
+
+    @Test
+    public void lengthOfLongestSubstring() {
+       Assert.assertEquals(3, Solution3.lengthOfLongestSubstring("pwwkew"));
+    }
+}
